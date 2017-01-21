74795721dde263b9c9fd2d309a9a8cd823840d57
haotao.lai <haotao.lai@gmail.com>
2017-01-21 10:34:56 -0500

    take and save photo

--- app/src/main/java/com/example/eric_lai/androidhelloworld/activity/MainActivity.java
+++ app/src/main/java/com/example/eric_lai/androidhelloworld/activity/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.eric_lai.androidhelloworld.activity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import com.example.eric_lai.androidhelloworld.R;
-import com.example.eric_lai.androidhelloworld.photo.AlbumStorageDirFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,97 +18,75 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private static final String JPEG_FILE_PREFIX = "IMG_";
-    private static final String JPEG_FILE_SUFFIX = ".jpg";
-    private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
-    private ImageView mImageView;
-
-
-    private String getAlbumName() {
-        return getString(R.string.album_name);
-    }
+    private static final String TAG = "MyActivity";
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView = (ImageView) findViewById(R.id.imageView1);
-    }
-
-    public void uploadPhoto(View view) {
-
-    }
-
-    public void takePhoto(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            galleryAddPic();
         }
     }
 
-//    private void dispatchTakePictureIntent(int actionCode) {
-//
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//
-//        switch(actionCode) {
-//            case ACTION_TAKE_PHOTO_B:
-//                File f = null;
-//
-//                try {
-//                    f = setUpPhotoFile();
-//                    mCurrentPhotoPath = f.getAbsolutePath();
-//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    f = null;
-//                    mCurrentPhotoPath = null;
-//                }
-//                break;
-//
-//            default:
-//                break;
-//        } // switch
-//
-//        startActivityForResult(takePictureIntent, actionCode);
-//    }
+    public void uploadPhoto(View view) {
+        // 这里添加启动你的activity的逻辑
+    }
+
+    public void takePhoto(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
-        File albumF = getAlbumDir();
-        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
-        return imageF;
-    }
-
-    private File getAlbumDir() {
-        File storageDir = null;
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
-            if (storageDir != null) {
-                if (! storageDir.mkdirs()) {
-                    if (! storageDir.exists()){
-                        Log.d("CameraSample", "failed to create directory");
-                        return null;
-                    }
-                }
-            }
-
-        } else {
-            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
-        }
-        return storageDir;
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.v(TAG, "photo is saved in: " + mCurrentPhotoPath);
+        return image;
     }
 }
--- app/src/main/java/com/example/eric_lai/androidhelloworld/photo/AlbumStorageDirFactory.java
+++
@@ -1,7 +0,0 @@
-package com.example.eric_lai.androidhelloworld.photo;
-
-import java.io.File;
-
-public abstract class AlbumStorageDirFactory {
-	public abstract File getAlbumStorageDir(String albumName);
-}
